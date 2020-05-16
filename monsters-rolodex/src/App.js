e0f53016219cb57b8de6fee7cb46eb66e0e78298
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-05-16 11:31:40 +0300

    !

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hellloooo</p>
+        <p>Hellloooo!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
